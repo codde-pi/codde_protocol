fb4ecf989fd4b491b5f3236e441c3b84a5076e53
Mathis Lecomte <vm.lecomte@gmail.com>
2024-02-21 20:21:21 +0100

    [CI] Change deps and cwd

---
+++ packages/flutter_codde_protocol/test/lib_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+void main() {
+  // TODO: mock socket server
+  group('Dart client', () {
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    // TODO: send and receive some Registry data
+    test('First Test', () {});
+  });
+}
