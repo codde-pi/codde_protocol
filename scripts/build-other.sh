8728c4788937f2b8d962b7ba17b37884b793c27f
Mathis Lecomte <vm.lecomte@gmail.com>
2024-03-29 13:59:50 +0100

    [FIX] Build for linux with new architecture

--- scripts/build-other.sh
+++ scripts/build-other.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Setup
-BUILD_DIR=platform-build
+ROOT=/home/matt/Projects/codde_pi/codde_protocol
+BUILD_DIR="$ROOT/platform-build"
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
@@ -9,6 +10,8 @@ cd $BUILD_DIR
 cargo install cargo-zigbuild
 
 zig_build() {
+	cd "$ROOT/packages/codde_protocol/native/client" || exit
+
 	local TARGET="$1"
 	local PLATFORM_NAME="$2"
 	local LIBNAME="$3"
@@ -16,19 +19,22 @@ zig_build() {
 	# export PYO3_CROSS_PYTHON_VERSION=3.11
 	# export PYO3_CROSS_LIB_DIR="../target/$TARGET/release/"
 	rustup target add "$TARGET"
-	cargo zigbuild --target "$TARGET" -r
+	cargo zigbuild --target "$TARGET" -r --target-dir target
 	# TODO: raise error if LIB is not found
-	mkdir "$PLATFORM_NAME"
-	cp "../target/$TARGET/release/$LIBNAME" "$PLATFORM_NAME/"
+	mkdir "$BUILD_DIR/$PLATFORM_NAME"
+	cp "target/$TARGET/release/$LIBNAME" "$BUILD_DIR/$PLATFORM_NAME/"
 }
 
 # Build all the dynamic libraries
 LINUX_LIBNAME=libcodde_protocol.so
 zig_build aarch64-unknown-linux-gnu linux-arm64 $LINUX_LIBNAME
 zig_build x86_64-unknown-linux-gnu linux-x64 $LINUX_LIBNAME
-WINDOWS_LIBNAME=library_name.dll
+WINDOWS_LIBNAME=libcodde_protocol.dll
+# TODO: win_build
 win_build aarch64-pc-windows-msvc windows-arm64 $WINDOWS_LIBNAME
 win_build x86_64-pc-windows-msvc windows-x64 $WINDOWS_LIBNAME
+
+cd $BUILD_DIR || exit
 
 # Archive the dynamic libs
 tar -czvf other.tar.gz linux-* windows-*
