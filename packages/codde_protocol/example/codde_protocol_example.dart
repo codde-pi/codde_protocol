be7b500a2255ccc5c0a0ce04b56df367870dcc0c
Mathis Lecomte <vm.lecomte@gmail.com>
2024-04-08 16:39:17 +0200

    [TEST] Fix test deps and integration test methods
    - [CLEAN] Dart code analysis

--- packages/codde_protocol/example/codde_protocol_example.dart
+++ packages/codde_protocol/example/codde_protocol_example.dart
@@ -5,8 +5,7 @@ final frame = Frame(id: 1, data: data);
 
 void main(List<String> args) async {
   await RustLib.init();
-  final client =
-      await ComSocketClient.newComSocketClient(address: "localhost:12345");
+  final client = ComSocketClient(address: "localhost:12345");
   client.connect();
   client.send(data: frame);
   client.disconnect();
