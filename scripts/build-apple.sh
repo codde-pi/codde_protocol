90db3b81e94873290d11bf5c8bc504ee3e09c0f4
Mathis Lecomte <vm.lecomte@gmail.com>
2024-04-08 12:40:22 +0200

    [CI] Fix build apple script
    - [CI] Use arm64 image for android tests

--- scripts/build-apple.sh
+++ scripts/build-apple.sh
@@ -22,15 +22,15 @@ mkdir mac-lipo ios-sim-lipo
 IOS_SIM_LIPO=ios-sim-lipo/$LIBNAME
 MAC_LIPO=mac-lipo/$LIBNAME
 lipo -create -output $IOS_SIM_LIPO \
-	../target/aarch64-apple-ios-sim/release/$LIBNAME \
-	../target/x86_64-apple-ios/release/$LIBNAME
+	target/aarch64-apple-ios-sim/release/$LIBNAME \
+	target/x86_64-apple-ios/release/$LIBNAME
 lipo -create -output $MAC_LIPO \
-	../target/aarch64-apple-darwin/release/$LIBNAME \
-	../target/x86_64-apple-darwin/release/$LIBNAME
+	target/aarch64-apple-darwin/release/$LIBNAME \
+	target/x86_64-apple-darwin/release/$LIBNAME
 xcodebuild -create-xcframework \
 	-library $IOS_SIM_LIPO \
 	-library $MAC_LIPO \
-	-library ../target/aarch64-apple-ios/release/$LIBNAME \
+	-library target/aarch64-apple-ios/release/$LIBNAME \
 	-output $FRAMEWORK
 zip -r $FRAMEWORK.zip $FRAMEWORK
 
