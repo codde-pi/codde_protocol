401bdeb462269378bedc664616bb569e4082f5fb
Mathis Lecomte <vm.lecomte@gmail.com>
2024-04-08 17:14:41 +0200

    [WIP] No hope in fixes

--- scripts/build-apple.sh
+++ scripts/build-apple.sh
@@ -5,22 +5,21 @@ ROOT="$(pwd)"
 BUILD_DIR="$ROOT/platform-build"
 mkdir $BUILD_DIR
 
-# Build the android libraries in the jniLibs directory
 cd "$ROOT/packages/codde_protocol/native/client" || exit
 # Build static libs
 for TARGET in \
 	aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim \
 	x86_64-apple-darwin aarch64-apple-darwin; do
 	rustup target add $TARGET
-	cargo build -r --target=$TARGET
+	cargo build -r --target=$TARGET --target-dir=target
 done
 
 # Create XCFramework zip
 FRAMEWORK="CoddeProtocol.xcframework"
 LIBNAME=libcodde_protocol.a
 mkdir mac-lipo ios-sim-lipo
-IOS_SIM_LIPO=ios-sim-lipo/$LIBNAME
 MAC_LIPO=mac-lipo/$LIBNAME
+IOS_SIM_LIPO=ios-sim-lipo/$LIBNAME
 lipo -create -output $IOS_SIM_LIPO \
 	target/aarch64-apple-ios-sim/release/$LIBNAME \
 	target/x86_64-apple-ios/release/$LIBNAME
