758b654f1b4c0a167c7a74be4d0266902f8d9d49
Mathis Lecomte <vm.lecomte@gmail.com>
2024-04-12 11:33:26 +0200

    [TEST] Fix test deps

--- packages/flutter_codde_protocol/example/test/example_test.dart
+++ packages/flutter_codde_protocol/example/test/example_test.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_codde_protocol/flutter_codde_protocol.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 
 void main() {
   setUp(() async {
     await RustLib.init(
-        externalLibrary:
-            ExternalLibrary.open("../../../target/debug/libcodde_protocol.so"));
+        externalLibrary: ExternalLibrary.open(
+            "../../codde_protocol/native/client/target/release/libcodde_protocol.so"));
   });
   test('Client init', () async {
     final client = ComSocketClient(address: 'localhost:12345');
